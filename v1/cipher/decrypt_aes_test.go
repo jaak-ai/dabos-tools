5e3cdbb7422dd7b6c0d1dfc02145c2748a395e91
gbcards <jorge.mondalgo@jaak.ai>
2022-11-29 14:43:13 -0500

    feat(QS-724): added spacing in tests

--- v1/cipher/decrypt_aes_test.go
+++ v1/cipher/decrypt_aes_test.go
@@ -12,6 +12,7 @@ func TestDecryptAES(t *testing.T) {
 	ciphertext := []byte{
 		190, 76, 59, 84, 128, 123, 194, 2, 189, 202, 30, 20, 97, 217, 90, 83, 51, 242, 162, 64, 128, 128, 169, 229, 189, 204, 235, 210, 89, 38, 186, 185,
 	}
+
 	// act or when
 	plaintext, err := DecryptAES(key, initializationVector, ciphertext)
 
--- v1/cipher/encrypt_aes_test.go
+++ v1/cipher/encrypt_aes_test.go
@@ -14,10 +14,10 @@ func TestEncryptAES(t *testing.T) {
 	// act or when
 	ciphertext, err := EncryptAES(key, initializationVector, plaintext)
 
+	// assertion or then
 	expected := []byte{
 		190, 76, 59, 84, 128, 123, 194, 2, 189, 202, 30, 20, 97, 217, 90, 83, 51, 242, 162, 64, 128, 128, 169, 229, 189, 204, 235, 210, 89, 38, 186, 185,
 	}
-	// assertion or then
 	assert.Nil(t, err)
 	assert.Equal(t, expected, ciphertext)
 }
